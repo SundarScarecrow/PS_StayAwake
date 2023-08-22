8d7c4a2680bb2636aba7cf770ad02f346329c694
SundarScarecrow <82038203+SundarScarecrow@users.noreply.github.com>
2023-08-22 12:22:39 +0530

    Add files via upload

---
+++ StayAwake.ps1
@@ -0,0 +1,13 @@
+﻿$minutes=Read-host "Enter number of minutes to stay awake"
+
+#write ".. screen will be awake for $minutes minutes"
+$myshell= New-Object -ComObject "Wscript.Shell"
+
+for($i=0;$i -lt $minutes; $i++)
+{
+
+write ".. $i minutes passed by.."
+Start-Sleep -Seconds 60
+$myshell.sendkeys("{F15}")
+
+}
